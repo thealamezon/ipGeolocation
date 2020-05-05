ac76c843458f31fe387a0f430e6ef7befadf11ba
thealamezon <59203105+thealamezon@users.noreply.github.com>
2020-05-05 12:40:42 +0400

    Create geo.js

---
+++ geo.js
@@ -0,0 +1,15 @@
+function ipLookUp () {
+  $.ajax('http://ip-api.com/json')
+  .then(
+      function success(response) {
+          console.log('User\'s Location Data is ', response);
+          console.log('User\'s Country', response.country);
+      },
+
+      function fail(data, status) {
+          console.log('Request failed.  Returned status of',
+                      status);
+      }
+  );
+}
+ipLookUp()
